58210ca5431a55f5c68df813a5c531f245fab8de
Rachid Khalidi rkhal@yahoo.fr <rkhal@yahoo.fr>
2020-04-25 17:02:12 +0100

    changed font size

--- app/controllers/perfumes_controller.rb
+++ app/controllers/perfumes_controller.rb
@@ -3,9 +3,15 @@ class PerfumesController < ApplicationController
 
   def index
     @perfumes = Perfume.all
+    if params[:query].present?
+      @perfumes = Perfume.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @perfumes = Perfume.all
+    end
   end
 
   def show
     @perfume = Perfume.find(params[:id])
   end
 end
+
